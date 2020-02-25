2126bbc1e06b7f65bacbb877d2863f1306231404
Tuấn Anh <tuananhvxl@gmail.com>
2020-02-26 00:39:48 +0700

    Update taLibs_ASR_utils.py

--- taLibs_ASR_utils.py
+++ taLibs_ASR_utils.py
@@ -87,16 +87,68 @@ class ASRutil():
         if save:plt.savefig('results/{}.svg'.format(thisModel_Name))
         plt.show()
 
-# import urllib.request  
-# import importlib
-
+# import urllib  
+# import importlib,os
 # url="https://github.com/holianh/Speech_Recognition_LIS_Net/raw/master/taLibs_ASR_utils.py"
 # filename=url.split('/')[-1]
-# print(filename) 
+# os.remove(filename)
+# print('fn=',filename) 
 # filename, headers = urllib.request.urlretrieve(url,filename=filename) 
-# from taLibs_ASR_utils import *
+# from  taLibs_ASR_utils import * 
 # importlib.reload(taLibs_ASR_utils)
-
+ 
+ 
 # stime=tt().strftime('%Y-%m-%d  %H:%M:%S')   #'2020-02-23  00:08:27' 
-# ut=ASRutil(thisModel_Name)
-# ut.help()
+# clSaveLoadPlot=ASRutil(thisModel_Name)
+# clSaveLoadPlot.help(), stime
+
+##############################################################################
+##############################################################################
+
+def ModuleReload(modulename):
+    import importlib
+    importlib.reload(modulename)
+    
+import os
+try:
+    import gpustat
+except:
+    os.system("pip install gpustat")
+    import gpustat
+def taLibs_getBatch_ViaGPU_Size(B):
+    s0=gpustat.new_query()
+    GPU_GB=s0.gpus[0].memory_total//1000
+    MB=list(B.keys())
+    k=0;
+    while(MB[k]<GPU_GB):k+=1
+    return B[MB[k]]
+    
+# Uses: GPU Size: Batch Size ------------------
+B={ 6 :16,
+    8 :24,
+    11:32,
+    12:40,
+    15:48,
+    16:64,
+    24:128}
+batch_size=taLibs_getBatch_ViaGPU_Size(B) 
+
+##  insert into cell in jupyter: -----------------------------------------------------------------
+# url="https://github.com/holianh/Linux_DeepLearning_tools/raw/master/taLibs_getBatch_ViaGPU_Size.py"
+# import urllib.request  
+# filename, headers = urllib.request.urlretrieve(url,filename=url.split('/')[-1]) 
+
+# import importlib
+# import taLibs_getBatch_ViaGPU_Size as getBatch
+# importlib.reload(getBatch)
+
+# # GPU Size: Batch Size
+# B={ 6 :16,
+#     8 :24,
+#     11:32,
+#     12:40,
+#     15:48,
+#     16:64,
+#     24:128}
+# batch_size=getBatch.taLibs_getBatch_ViaGPU_Size(B)
+# batch_size
